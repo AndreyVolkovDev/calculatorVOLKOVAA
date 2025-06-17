17695807124bbfa18162de32dce6e456b274d972
AndreyVolkovDev <154226271+AndreyVolkovDev@users.noreply.github.com>
2025-06-17 12:15:18 +0300

    Delete calculatorVOLKOVAA directory

--- calculatorVOLKOVAA/src/model/CalculationEntry.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-// Используем record для неизменяемого класса-хранилища данных
-public record CalculationEntry(String expression, String result) {
-    @Override
-    public String toString() {
-        return "Выражение: " + expression + " -> Результат: " + result;
-    }
-}
